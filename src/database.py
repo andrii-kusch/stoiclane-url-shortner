b81fbea84e2ea041035f47c8b5deed16613f404e
andrii-kusch <128434789+andrii-kusch@users.noreply.github.com>
2026-02-16 18:02:18 -0600

    Print every SQL statement SQLAlchemy sends to the database

--- src/database.py
+++ src/database.py
@@ -5,7 +5,8 @@ DATABASE_URL = "sqlite:///./urls.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}  # required for SQLite
+    connect_args={"check_same_thread": False},
+    echo=True
 )
 
 SessionLocal = sessionmaker(
